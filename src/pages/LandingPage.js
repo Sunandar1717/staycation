2c113e5b4592ecdd217ce23d81e422f6a48f2f76
Sunandar1717 <sunandar.developer@gmail.com>
2022-01-15 16:47:56 +0700

    menambahkan component hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import landingPage from 'Json/landingPage.json'
+import Header from 'parts/Header';
+import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked'
 
-import Header from 'parts/Header'
 export default class LandingPage extends Component {
     render() {
         return (
             <>
             <Header{...this.props}></Header>
+            <Hero data ={landingPage.hero}/>
+            <MostPicked data ={landingPage.mostPicked}/>
             </>
         )
     }
